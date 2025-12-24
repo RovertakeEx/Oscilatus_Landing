e79a5ce5d5fba5005b2140f795aba28f2eaef7cc
Rifnaz <hanifarifnazz@gmail.com>
2025-12-24 19:30:26 +0530

    Initiate the project

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+const root = createRoot(document.getElementById("root")!);
+
+root.render(
+  import.meta.env.DEV 
+  ? (
+     <StrictMode>
+      <App />
+    </StrictMode>
+  ) 
+  : (<App />)
+);
