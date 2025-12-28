d2912a747ea9010c4a97f90ced60fb4a0956770c
TharushiiDil <tharushiidilshanii@gmail.com>
2025-12-28 15:43:09 +0530

    Developed Approach and Benefits section

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Hero from "../components/Hero";
 import Challenge from '../components/Challenge';
+import Approach from '../components/Approach';
+import Benefits from '../components/Bebefits';
+
 
 const index = () => {
   return (
     <>
         <Hero />
         <Challenge />
+        <Approach/>
+        <Benefits/>
     </>
   )
 }
